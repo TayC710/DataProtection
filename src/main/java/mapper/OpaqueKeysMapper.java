aec34d0e12532f0e2f611b50265ddecf0214bef6
TayC710 <73151346+TayC710@users.noreply.github.com>
2024-12-04 14:32:48 +0800

    Delete src directory

--- src/main/java/mapper/OpaqueKeysMapper.java
+++
@@ -1,9 +0,0 @@
-package mapper;
-
-import org.apache.ibatis.annotations.Param;
-import pojo.OpaqueKeys;
-
-public interface OpaqueKeysMapper {
-    OpaqueKeys getOpaqueKeys();
-    int insertOpaqueKeys(@Param("serverPublicKey") byte[] serverPublicKey, @Param("serverPrivateKey") byte[] serverPrivateKey, @Param("oprfSeed") byte[] oprfSeed);
-}
